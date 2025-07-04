59253b3fd688441296c5af3521160cdb317c7053
imdudassir943 <163318610+imdudassir943@users.noreply.github.com>
2025-07-04 14:54:59 +0000

    type_converion

---
+++ JavaScripts_Basics/JS_Baseics_Data_Types.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/* data types in the javascripts
+number => 123
+string => "name"
+bigint => to store large number of data sets
+boolean => true/false
+null => stand alone value
+undefined => the value that is not defined in the scope or code
+symbol => unique
+*/
+
+// let score = "33a";
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(typeof isLoggedIn)
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+console.log(typeof someNumber)
